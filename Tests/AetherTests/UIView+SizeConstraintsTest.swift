9b07585cf39c7677babc40301d3a7f348745c332
Alexander Algashev <algashev@mail.ru>
2021-05-29 19:30:45 +0300

    Add test_setWidthConstraint_createsCorrectValues()

--- Tests/AetherTests/UIView+SizeConstraintsTest.swift
+++ Tests/AetherTests/UIView+SizeConstraintsTest.swift
@@ -1,6 +1,8 @@
 import XCTest
 @testable import Aether
 
+// MARK: - Width Constraint
+
 final class UIViewSizeConstraintsTest: XCTestCase {
     var sut: NSLayoutConstraint!
     
@@ -14,6 +16,26 @@ final class UIViewSizeConstraintsTest: XCTestCase {
         XCTAssertEqual(self.sut.relation, .equal)
         XCTAssertEqual(self.sut.priority, .required)
         XCTAssertTrue(self.sut.isActive)
+    }
+    
+    func test_setWidthConstraint_createsCorrectValues() {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        let width: CGFloat = 10
+        let relation: NSLayoutConstraint.Relation = .greaterThanOrEqual
+        let priority: UILayoutPriority = .defaultHigh
+        let isActive = false
+        
+        self.sut = view.setConstraint(
+            width: width,
+            relatedBy: relation,
+            priority: priority,
+            isActive: isActive)
+        
+        XCTAssertEqual(self.sut.constant, width)
+        XCTAssertEqual(self.sut.relation, relation)
+        XCTAssertEqual(self.sut.priority, priority)
+        XCTAssertEqual(self.sut.isActive, isActive)
     }
 
     
